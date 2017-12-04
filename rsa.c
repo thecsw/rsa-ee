966621080d7549af1cda4dab0220427b5a686245
thecsw <thecsmoothiew@yahoo.com>
2017-12-04 17:00:42 +0400

    Finished the main code

--- rsa.c
+++ rsa.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "gmp.h"
 
 int main(int argc, char** argv) {
-  if (argc != 3) {
-	printf("Please enter values of p and q. FILE [] []\nExiting...\n");
-	return 0;
+  if (argc != 5) {
+	printf("Please enter values of n, e and d, also the message.\nExiting...\n");
+	abort();
   }
-  mpz_t p, q, n, lam, e, d;
-  mpz_init(p);
-  mpz_set_str(p, argv[1], 10);
-  gmp_printf("%Zd\n", p);
+  mpz_t n, e, d, m, c, mt;
+  mpz_init(m);
+  mpz_init(c);
+  mpz_init(mt);
+  mpz_init_set_str(n, argv[1], 10);
+  mpz_init_set_str(e, argv[2], 10);
+  mpz_init_set_str(d, argv[3], 10);
+  mpz_import(m, strlen(argv[4]), 1, 1, 0, 0, argv[4]);
+  mpz_powm(c, m, e, n);
+
+  if (mpz_cmp(m, n) > 0) {
+	printf("The text value is bigger than modulo parameter.\nExiting...");
+	abort();
+  }
+
+  mpz_powm(mt, c, d, n);
+  char text[64];
+  mpz_export(text, NULL, 1, 1, 0, 0, mt);
+  printf("Initial settings:");
+  gmp_printf("The n value: %Zd\n", n);
+  gmp_printf("The e value: %Zd\n", e);
+  gmp_printf("The d value: %Zd\n", d);
+ 
+  printf("\tInput text: %s\n", argv[4]);
+  gmp_printf("\tConverted text : %Zd\n", m);
+  gmp_printf("\tEncrypted text : %Zd\n", c);
+  gmp_printf("\tDecrypted text : %Zd\n", mt);
+  printf("\tOutput text - %s\n", text);
+
+
   return 0;
 }
