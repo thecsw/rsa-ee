c344de86acb08a5a211c14f50a96bcaff8baca21
thecsw <thecsmoothiew@yahoo.com>
2017-12-08 00:42:53 +0400

    Output using GMP library

--- rsa.c
+++ rsa.c
@@ -21,7 +21,6 @@ int main(int argc, char** argv) {
   mpz_init(m);
   mpz_init(c);
   mpz_init(mt);
-
   // Initializing and setting variables with values from the terminal input
   mpz_init_set_str(n, argv[1], 10);
   mpz_init_set_str(e, argv[2], 10);
@@ -38,36 +37,36 @@ int main(int argc, char** argv) {
   mpz_powm(c, m, e, n); // c = pow(m, e) % n
 
   // Trying to make the cypher into a text
-  char enc[64];
+  char enc[256];
   mpz_export(enc, NULL, 1, 1, 0, 0, c);
 
   // Decrypting the message
   mpz_powm(mt, c, d, n); // mt = pow(c, d) % n
 
   // Converting the message from digits to characters
-  char text[64];
+  char text[256];
   mpz_export(text, NULL, 1, 1, 0, 0, mt);
 
   // Print out all the necessary data 
-  printf("\nInitial settings:\n");
+  gmp_printf("\nInitial settings:\n");
   gmp_printf("\tThe n value: %Zd\n", n);
   gmp_printf("\tThe e value: %Zd\n", e);
   gmp_printf("\tThe d value: %Zd\n", d);
 
-  printf("\nAll following data will be shown as hexadecimal values.");
-  printf("\nIt's easier to present in this way than decimal.\n");
+  gmp_printf("\nAll following data will be shown as hexadecimal values.");
+  gmp_printf("\nIt's easier to present in this way than decimal.\n");
   
-  printf("\nRSA keys:\n");
+  gmp_printf("\nRSA keys:\n");
   gmp_printf("\tPublic key  :   (%#Zx, \n\t\t\t %#Zx)\n", e, n);
   gmp_printf("\tPrivate key :   (%#Zx, \n\t\t\t %#Zx)\n", d, n);
   
-  printf("\nThe results:\n");
-  printf("\tInput text :     %s\n", argv[4]);
+  gmp_printf("\nThe results:\n");
+  gmp_printf("\tInput text :     %s\n", argv[4]);
   gmp_printf("\tConverted text : %Zx\n", m);
   gmp_printf("\tEncrypted text : %Zx\n", c);
-  printf("\tEncrypted text : %s\n", enc);
+  gmp_printf("\tEncrypted text : %s\n", enc);
   gmp_printf("\tDecrypted text : %Zx\n", mt);
-  printf("\tOutput text :    %s\n", text);
+  gmp_printf("\tOutput text :    %s\n", text);
 
   // Clearing the memory and resetting it to NULL
   mpz_clears(n, e, d, m, c, mt, NULL);
