8fcacde8bb751845ef5fb3bdb04d6848a165135d
thecsw <thecsmoothiew@yahoo.com>
2017-12-08 00:50:10 +0400

    Added some log files

--- rsa.c
+++ rsa.c
@@ -4,48 +4,66 @@
 #include "gmp.h"
 
 int main(int argc, char** argv) {
-
+  gmp_printf("Checking for the terminal input...\n");
   // We need exactly 5 variables. If not, abort.
   if (argc != 5) {
 	printf("Please enter values of n, e and d, also the message.\nExiting...\n");
 	abort();
   }
-
+  gmp_printf("Success.\n");
+  
   // Declaring varibles, ned is obvious, while:
   // m is the original converted message
   // c is the original converted message that is encrypted
   // mt is the decrypted original converted message that was encrypted 
+  gmp_printf("Creating local variables...\n");
   mpz_t n, e, d, m, c, mt;
-
+  gmp_printf("Success.\n");
+  
   // Initializing variables, thus allocating memery for them
+  gmp_printf("Initializing local variables...\n");
   mpz_init(m);
   mpz_init(c);
   mpz_init(mt);
+  gmp_printf("Success.\n");
   // Initializing and setting variables with values from the terminal input
+  gmp_printf("Initializing and setting up local variables...\n");
   mpz_init_set_str(n, argv[1], 10);
   mpz_init_set_str(e, argv[2], 10);
   mpz_init_set_str(d, argv[3], 10);
+  gmp_printf("Success.\n");
   mpz_import(m, strlen(argv[4]), 1, 1, 0, 0, argv[4]);
-
+  gmp_printf("Success.\n");
+  
   // If the converted message's value is bigger than n, abort.
+  gmp_printf("Checking if text value is bigger than the modulo...\n");
   if (mpz_cmp(m, n) > 0) {
 	printf("The text value is bigger than modulo parameter.\nExiting...");
 	abort();
   }
+  gmp_printf("Success.\n");
   
   // Encrypting the message
+  gmp_printf("Encrypting the message...\n");
   mpz_powm(c, m, e, n); // c = pow(m, e) % n
-
+  gmp_printf("Success.\n");
+  
   // Trying to make the cypher into a text
+  gmp_printf("Trying to directly translate the encrypted text...\n");
   char enc[256];
   mpz_export(enc, NULL, 1, 1, 0, 0, c);
-
+  gmp_printf("Success.\n");
+  
   // Decrypting the message
+  gmp_printf("Decrypting the message...\n");
   mpz_powm(mt, c, d, n); // mt = pow(c, d) % n
-
+  gmp_printf("Success.\n");
+  
   // Converting the message from digits to characters
+  gmp_printf("Translating the decypted text into string...\n");
   char text[256];
   mpz_export(text, NULL, 1, 1, 0, 0, mt);
+  gmp_printf("Success.\n");
 
   // Print out all the necessary data 
   gmp_printf("\nInitial settings:\n");
