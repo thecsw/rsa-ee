019255db7a3a828d88bfdf4f607c6b4e3856322c
thecsw <thecsmoothiew@yahoo.com>
2017-12-17 12:55:47 +0400

    Numbers do not repeat

--- break.c
+++ break.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+void not(){}
+int find_val(int* arr, int size, int val) {
+	int i = 0;
+	for (i = 0; i < size; i++) {
+		if (arr[i] == val) return 1;
+	}
+	return 0;
+}
 
 void brute_force(int n) {
-	int p, q;
+	int p, q, i = 0;
+	int arr[100];
 	for (p = 2; p < n; p++) {
 		for (q = 2; q < n; q++) {
-			if (p * q == n) {
+			if ((p * q == n) && (find_val(arr, i, p) == 0)) {
 			    printf("Primes for %d are : %d and %d\n", n, p, q);
-				return;
+				arr[i++] = p;
+				arr[i++] = q;
 			}
 		}
 	}
