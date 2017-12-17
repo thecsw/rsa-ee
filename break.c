eb9e5afd84033241b766d92011e50ff30783b3f3
thecsw <thecsmoothiew@yahoo.com>
2017-12-18 02:36:03 +0400

    Change all int to unsgined int.

--- break.c
+++ break.c
@@ -3,28 +3,28 @@
 #include <time.h>
 #include <math.h>
 void not(){}
-void find_primes(int* arr, int size) {
-	int i = 0;
+void find_primes(unsigned int* arr, unsigned int size) {
+	unsigned int i = 0;
 	for (i = 0; i < size; i++) arr[i] = i;
 	for (i = 2; i < size; i++) {
 		if (arr[i] != 0) {
-			int j = i;
+			unsigned int j = i;
 			while (j + arr[i] < size) arr[j += arr[i]] = 0;
 		}
 	}
 }
 
-int count(int* arr, int size) {
-	int i, k = 0;
+int count(unsigned int* arr, unsigned int size) {
+	unsigned int i, k = 0;
 	for (i = 0; i < size; i++) {
 		if (arr[i] != 0) k++;
 	}
 	return k;
 }
 
-void primes(int* arr, int size, int k, int* arg) {
-	int i, l = 0;
-	int j = 0;
+void primes(unsigned int* arr, unsigned int size, unsigned int k, unsigned int* arg) {
+	unsigned int i, l = 0;
+	unsigned int j = 0;
 	for (i = 0; i < size; i++) {
 		if (arr[i] != 0) {
 			arg[j++] = arr[i];
@@ -32,18 +32,18 @@ void primes(int* arr, int size, int k, int* arg) {
 	}
 }
 
-void brute_force(int n) {
-	int p, q, i = 0;
-	int* arr = (int*)malloc(sizeof(int)*n);
+void brute_force(unsigned int n) {
+	unsigned int p, q, i = 0;
+	unsigned int* arr = (unsigned int*)malloc(sizeof(int)*n);
 	find_primes(arr, n);
-	int s = count(arr, n);
-	int* arg = (int*)malloc(sizeof(int)*s);
+	unsigned int s = count(arr, n);
+	unsigned int* arg = (unsigned int*)malloc(sizeof(int)*s);
 	primes(arr, n, s, arg);
 	free(arr);
 	for (p = 0; p < s; p++) {
 		for (q = 0; q < s; q++) {
 			if (arg[p] * arg[q] == n) {
-				printf("Primes are : %d and %d", arg[p], arg[q]);
+				printf("Primes are : %u and %u", arg[p], arg[q]);
 				free(arg);
 				return;
 			}
@@ -57,11 +57,11 @@ int main(int argc, char** argv) {
 		return EXIT_FAILURE;
 	}
 	clock_t begin = clock();
-	int n;
+	unsigned int n;
 	n = atoi(argv[1]);
 	double secs = (pow(n/log(n), 2)) / CLOCKS_PER_SEC;
-	int years = ceil(secs / (3600 * 24 * 365));
-	printf("In worst-case scenario, it will take %f seconds or %d years\n", secs, years);
+	int days = ceil(secs / (3600 * 24));
+	printf("In worst-case scenario, it will take %f seconds or %d day(s)\n", secs, days);
 	brute_force(n);
 	clock_t end = clock();
 	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
