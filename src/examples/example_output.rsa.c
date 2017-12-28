870dfc510fe725eb1b78d4ad1c2f55ce29e281b4
thecsw <thecsmoothiew@yahoo.com>
2017-12-28 20:04:30 +0400

    Appropriate folder

---
+++ src/examples/example_output.rsa.c
@@ -0,0 +1,27 @@
+$ ./a.out 9516311845790656153499716760847001433441357 65537 5617843187844953170308463622230283376298685 "The cake is a lie."       
+
+Initial settings:
+	The n value: 9516311845790656153499716760847001433441357
+	The e value: 65537
+	The d value: 5617843187844953170308463622230283376298685
+
+All following data will be shown as hexadecimal values.
+It's easier to present in this way than decimal.
+
+RSA keys:
+	Public key  : (0x10001, 
+		       0x6d3ded5264bdacea5cc076e62ae5676c844d)
+	Private key : (0x407d5b79d59107e07e4086752d72897e8abd, 
+		       0x6d3ded5264bdacea5cc076e62ae5676c844d)
+
+The results:
+	Input text :     The cake is a lie.
+	Converted text : 5468652063616b652069732061206c69652e
+	Encrypted text : 639e13dc8bb6d382af7c1480c37476f7e4e2
+	Encrypted text : c�܋�ӂ�|��tv��
+	Decrypted text : 5468652063616b652069732061206c69652e
+	Output text :    The cake is a lie.
+
+Thank you for using rsa-ee!
+Exiting...
+
