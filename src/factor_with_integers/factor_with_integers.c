80a6cbe085d2bb6d30e10fbfb7879c689f857046
thecsw <sagindyk.urazayev@protonmail.ch>
2018-02-23 21:57:54 +0400

    Formatting

--- src/factor_with_integers/factor_with_integers.c
+++ src/factor_with_integers/factor_with_integers.c
@@ -1,25 +1,29 @@
-#include<stdio.h>
-#include<time.h>
+#include <stdio.h>
 #include <stdlib.h>
-void not(){}
-int find_val(int* arr, int size, int val) {
+#include <time.h>
+void not() {}
+int find_val(int *arr, int size, int val) {
 	int i = 0;
-	for(i = 0; i < size; i++) if(arr[i] == val) return 1;
+	for (i = 0; i < size; i++)
+		if (arr[i] == val)
+			return 1;
 	return 0;
 }
 
 void brute_force(int n) {
 	int p, q, i = 0;
 	int arr[100];
-	for(p = 2; p < n; p++) for(q = 2; q < n; q++) if ((p * q == n) && (find_val(arr, i, p) == 0)) {
+	for (p = 2; p < n; p++)
+		for (q = 2; q < n; q++)
+			if ((p * q == n) && (find_val(arr, i, p) == 0)) {
 				printf("Primes for %d are : %d and %d\n", n, p, q);
 				arr[i++] = p;
 				arr[i++] = q;
 			}
 }
 
-int main(int argc, char** argv) {
-	if(argc != 2) {
+int main(int argc, char **argv) {
+	if (argc != 2) {
 		printf("Please enter n.\n");
 		return EXIT_FAILURE;
 	}
