b44e39779fe602a63f71dc0895c2e918042eb6f3
thecsw <sagindyk.urazayev@protonmail.ch>
2018-03-13 21:15:59 +0400

    Just did a more generic and common full brute force

--- src/factor_with_integers/factor_with_integers.c
+++ src/factor_with_integers/factor_with_integers.c
@@ -3,24 +3,15 @@
 #include <time.h>
 void not() {}
 
-int find_val(int *arr, int size, int val) {
-	int i = 0;
-	for (i = 0; i < size; i++)
-		if (arr[i] == val)
-			return 1;
-	return 0;
-}
-
 void brute_force(int n) {
-	int p, q, i = 0;
-	int arr[100];
+	int p, q = 0;
 	for (p = 2; p < n; p++)
 		for (q = 2; q < n; q++)
-			if ((p * q == n) && (find_val(arr, i, p) == 0)) {
+			if ((p * q == n) && (n / p == q)) {
 				printf("Primes for %d are : %d and %d\n", n, p, q);
-				arr[i++] = p;
-				arr[i++] = q;
+				return;
 			}
+	printf("The given modulo is a prime. Bailing out...\n");
 }
 
 int main(int argc, char **argv) {
